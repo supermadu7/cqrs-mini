d44a1c8644945d3c822335281660613b7614b530
supermadu7 <hi.madu@yahoo.com>
2025-06-17 17:48:00 +0100

    update route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CatalogController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/order', [OrderController::class, 'store']);
+Route::get('/catalog', [CatalogController::class, 'index']);
+
